1c54034de33f5bbe8ae0e869ded0a00e5575f297
vitostamatti <vitostamatti@gmail.com>
2023-12-04 19:36:12 +0100

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Vito Stamatti on 4/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
